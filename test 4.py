6d1ff076e359c377973c72d5898738997d94d0a9
BuvleGum <117893334+ameliaaftiana@users.noreply.github.com>
2023-02-20 13:23:00 +0700

    Update test 4.py

--- test 4.py
+++ test 4.py
@@ -1,6 +1,6 @@
-nama1=input("Masukkan nama ")
-nama2=input("Masukkan nama ")
-nama3=input("Masukkan nama ")
+nama1=input("Masukkan nama: ")
+nama2=input("Masukkan nama: ")
+nama3=input("Masukkan nama: ")
 
 a=len(nama1)
 b=len(nama2)
@@ -8,4 +8,10 @@ c=len(nama3)
 
 urut=[a,b,c]
 urut.sort()
-print(urut[1])
+
+if urut[1]==a:
+    print("Jadi yang memiliki nama terpanjang adalah",nama1)
+elif urut[1]==b:
+    print("Jadi yang memiliki nama terpanjang adalah",nama2)
+else:
+    print("Jadi yang memiliki nama terpanjang adalah", nama3)
